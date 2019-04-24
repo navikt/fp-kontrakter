bdb4beca6c8dae5ba91368a3e8ee6901e3f4c7ac
Marius Bækken Glittum <ekunamatata@gmail.com>
2019-04-24 10:53:25 +0200

    Etablerer et manifest over topics for familieområdet (#25)

---
+++ fp-topics/src/main/java/no/nav/familie/topic/TopicManifest.java
@@ -0,0 +1,20 @@
+package no.nav.familie.topic;
+
+import org.apache.kafka.common.serialization.Serdes;
+
+/**
+ * Manifest over topics for familieområdet
+ */
+public final class TopicManifest {
+    /**
+     * Publiseres vedtak og deres anvisninger som lagres så i et ytelselager.
+     */
+    public static final Topic FATTET_VEDTAK = new Topic("privat-foreldrepenger-vedtatt-fattet-hendelse-fpabakus", Serdes.String(), Serdes.String());
+    /**
+     * Publiserer hendelser ved endringer i aksjonspunkt. Benyttes for oppgavestyring
+     */
+    public static final Topic AKSJONSPUNKT_HENDELSE = new Topic("privat-foreldrepenger-aksjonspunkthendelse-fpsak", Serdes.String(), Serdes.String());
+
+    private TopicManifest() {
+    }
+}
