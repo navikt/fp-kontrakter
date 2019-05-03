a20bf5e385c7ed7aff4b87fac1370c6972117935
Marius Glittum <Marius.Glittum@nav.no>
2019-05-03 12:14:32 +0200

    Legger på test av UUID-regex

--- vl-kontrakt-registerdata-callback/src/main/java/no/nav/foreldrepenger/kontrakter/callback/registerdata/ReferanseDto.java
+++ vl-kontrakt-registerdata-callback/src/main/java/no/nav/foreldrepenger/kontrakter/callback/registerdata/ReferanseDto.java
@@ -8,8 +8,10 @@ import javax.validation.constraints.Pattern;
  */
 public class ReferanseDto {
 
+    public static final String UUID_REGEX = "\\b[0-9a-f]{8}\\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\\b[0-9a-f]{12}\\b";
+
     @NotNull
-    @Pattern(regexp = "\\b[0-9a-f]{8}\\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\\b[0-9a-f]{12}\\b")
+    @Pattern(regexp = UUID_REGEX)
     private String referanse;
 
     public String getReferanse() {
