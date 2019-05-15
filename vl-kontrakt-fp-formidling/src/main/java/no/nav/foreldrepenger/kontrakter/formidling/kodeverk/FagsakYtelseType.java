9a585842997f29c51c56d82353ab01c5a048eefc
Ramesh Shiraddi <extrsi@users.noreply.github.com>
2019-05-15 10:54:40 +0200

    PFP-7777 Dto for forhåndsvisning av dokument i FPFormidling (#41)

---
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/kodeverk/FagsakYtelseType.java
@@ -0,0 +1,19 @@
+package no.nav.foreldrepenger.kontrakter.formidling.kodeverk;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class FagsakYtelseType extends Kodeverk {
+
+    public static final String DISCRIMINATOR = "FAGSAK_YTELSE"; //$NON-NLS-1$
+
+    public static final FagsakYtelseType ENGANGSTØNAD = new FagsakYtelseType("ES"); //$NON-NLS-1$
+    public static final FagsakYtelseType FORELDREPENGER = new FagsakYtelseType("FP"); //$NON-NLS-1$
+    public static final FagsakYtelseType SVANGERSKAPSPENGER = new FagsakYtelseType("SVP"); //$NON-NLS-1$
+
+    @JsonCreator
+    private FagsakYtelseType(@JsonProperty("kode") String kode) {
+        super(DISCRIMINATOR, kode);
+    }
+
+}
