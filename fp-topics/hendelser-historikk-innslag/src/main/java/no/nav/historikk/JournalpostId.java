c47b0b46c2b07979d10c57542a06ff38fb8af063
Aleksander Andresen <caeses@gmail.com>
2019-05-20 09:38:44 +0200

    Endrer journalpostid i historikk for deserialisering (#52)

--- fp-topics/hendelser-historikk-innslag/src/main/java/no/nav/historikk/JournalpostId.java
+++ fp-topics/hendelser-historikk-innslag/src/main/java/no/nav/historikk/JournalpostId.java
@@ -1,28 +1,25 @@
 package no.nav.historikk;
 
+import java.io.Serializable;
+
+import javax.validation.constraints.Pattern;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
-
-import java.io.Serializable;
-import java.util.regex.Pattern;
 
 /**
  * Journalpostid refererer til journalpost registret i Joark.
  */
 public class JournalpostId implements Serializable {
 
-    private static final String CHARS = "a-z0-9_:-";
-
-    private static final Pattern VALID = Pattern.compile("^(-?[1-9]|[a-z0])[" + CHARS + "]*$", Pattern.CASE_INSENSITIVE);
-    private static final Pattern INVALID = Pattern.compile("[^" + CHARS + "]+", Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
-
     @JsonValue
+    @Pattern(regexp = "^[\\p{L}\\p{N}_\\.\\-]+$")
     @JsonProperty(value = "journalpostId", required = true)
     private String journalpostId;
 
     @JsonCreator
-    public JournalpostId(@JsonProperty(value = "journalpostId", required = true) String journalpostId) {
+    public JournalpostId(String journalpostId) {
         this.journalpostId = journalpostId;
     }
 
