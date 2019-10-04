170954ea5136d0574498bad4a994ab641ded6998
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2019-10-04 16:17:00 +0200

    FIX fordel-fpsak støtte ny maxi søknad (#123)

--- vl-kontrakt-fordel/src/main/java/no/nav/foreldrepenger/kontrakter/fordel/JournalpostMottakDto.java
+++ vl-kontrakt-fordel/src/main/java/no/nav/foreldrepenger/kontrakter/fordel/JournalpostMottakDto.java
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class JournalpostMottakDto {
 
-    private static final int PAYLOAD_MAX_CHARS = 96000;
+    private static final int PAYLOAD_MAX_CHARS = 196000;
 
     @NotNull
     @Digits(integer = 18, fraction = 0)
