eea82624cb4962f5450601a7afddb2dc782ecba5
Michal J. Sladek <michal.sladek@nav.no>
2020-02-18 22:18:11 +0100

    Grensesnittavstemming kontrakt.

---
+++ vl-kontrakt-oekonomi/grensesnittavstemming-v1/src/main/java/no/nav/foreldrepenger/integrasjon/økonomistøtte/grensesnittavstemming/GrensesnittavstemmingSkjemaConstants.java
@@ -0,0 +1,12 @@
+package no.nav.foreldrepenger.integrasjon.økonomistøtte.grensesnittavstemming;
+
+import no.nav.foreldrepenger.integrasjon.økonomistøtte.grensesnittavstemming.Avstemmingsdata;
+
+public final class GrensesnittavstemmingSkjemaConstants {
+    public static final String NAMESPACE = "http://nav.no/virksomhet/tjenester/avstemming/meldinger/v1";
+    public static final String XSD_LOCATION = "xsd/grensesnittavstemmingskjema-v1.xsd";
+    public static final Class<Avstemmingsdata> JAXB_CLASS = Avstemmingsdata.class;
+
+    private GrensesnittavstemmingSkjemaConstants() {
+    }
+}
