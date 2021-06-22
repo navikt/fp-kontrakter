62e65aecaf55eeab6342c8977c264c87a851b53c
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2021-06-22 14:41:58 +0200

    TFP-4387 kontrakt for utflyttingshendelser

---
+++ vl-kontrakt-fp-abonnent/src/test/java/no/nav/foreldrepenger/kontrakter/abonnent/v2/UtflyttingHendelseDtoTest.java
@@ -0,0 +1,58 @@
+package no.nav.foreldrepenger.kontrakter.abonnent.v2;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+import javax.validation.Validation;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import no.nav.foreldrepenger.kontrakter.abonnent.v2.pdl.UtflyttingHendelseDto;
+
+public class UtflyttingHendelseDtoTest {
+
+    private static final ObjectWriter WRITER = TestJsonMapper.getMapper().writerWithDefaultPrettyPrinter();
+    private static final ObjectReader READER = TestJsonMapper.getMapper().reader();
+
+    private static final AktørIdDto AKTØR_ID = new AktørIdDto("10000000001");
+    private static final LocalDate NÅ = LocalDate.now();
+
+    @Test
+    public void skal_serialisere_og_deserialisere_utflyttinghendelse() throws Exception {
+        // Arrange
+        var hendelse = new UtflyttingHendelseDto();
+        hendelse.setId("id_1");
+        hendelse.setEndringstype(Endringstype.OPPRETTET);
+        hendelse.setAktørId(Collections.singletonList(AKTØR_ID));
+        hendelse.setUtflyttingsdato(NÅ);
+
+        // Act
+        var json = WRITER.writeValueAsString(hendelse);
+        //System.out.println(json);
+        var roundTripped = (HendelseDto) READER.forType(HendelseDto.class).readValue(json);
+        var roundTrippedCast = (UtflyttingHendelseDto) roundTripped;
+
+        // Assert
+        assertThat(roundTripped).isNotNull();
+        assertThat(roundTripped).isInstanceOf(UtflyttingHendelseDto.class);
+        assertThat((roundTripped).getHendelsetype()).isEqualTo(UtflyttingHendelseDto.HENDELSE_TYPE);
+        assertThat(roundTrippedCast.getId()).isEqualTo("id_1");
+        assertThat(roundTrippedCast.getAktørId().get(0)).isEqualTo(AKTØR_ID);
+        assertThat(roundTrippedCast.getUtflyttingsdato()).isEqualTo(NÅ);
+        validateResult(roundTripped);
+    }
+
+    private void validateResult(Object roundTripped) {
+        assertThat(roundTripped).isNotNull();
+        try (var factory = Validation.buildDefaultValidatorFactory()) {
+            var validator = factory.getValidator();
+            var violations = validator.validate(roundTripped);
+            assertThat(violations).isEmpty();
+        }
+    }
+}
