727825645ced956d93b0f0547298452266287b68
Petter Ekern <43809623+pekern@users.noreply.github.com>
2021-12-07 08:08:41 +0100

    TFP-4761: Kontrakt for å lagre faresignalvurdering i fprisk (#302)
    * TFP-4761: Kontrakt for å lagre faresignalvurdering i fprisk

---
+++ vl-kontrakt-fp-risk/src/main/java/no/nav/foreldrepenger/kontrakter/risk/kodeverk/FaresignalVurdering.java
@@ -0,0 +1,10 @@
+package no.nav.foreldrepenger.kontrakter.risk.kodeverk;
+
+public enum FaresignalVurdering {
+    INNVIRKNING,
+    INNVILGET_REDUSERT,
+    INNVILGET_UENDRET,
+    AVSLAG_FARESIGNAL,
+    AVSLAG_ANNET,
+    INGEN_INNVIRKNING,
+}
