fcc306ac50e19e93fdf1e625dcc768f74408c61f
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2022-01-18 11:02:17 +0100

    TFP-4708 Dtos for bestilling fra fpsak og enkel feedback om produksjon (#321)
    * Dtos for bestilling fra fpsak og enkel feedback om produksjon
    
    * Redusere klager fra bygg

---
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/kodeverk/YtelseType.java
@@ -0,0 +1,9 @@
+package no.nav.foreldrepenger.kontrakter.formidling.kodeverk;
+
+public enum YtelseType {
+
+    /** Kun Folketrygdloven K14 ytelser. */
+    ENGANGSTØNAD,
+    FORELDREPENGER,
+    SVANGERSKAPSPENGER
+}
--- vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v1/BehandlingUuidDto.java
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v1/BehandlingUuidDto.java
@@ -5,21 +5,7 @@ import java.util.UUID;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-public class BehandlingUuidDto {
-
-    public static final String NAME = "uuid";
-    public static final String DESC = "behandlingUUID";
-
-    @NotNull
-    @Valid
-    private UUID behandlingUuid;
-
-    public BehandlingUuidDto() { //For Jackson
-    }
-
-    public BehandlingUuidDto(UUID behandlingUuid) {
-        this.behandlingUuid = behandlingUuid;
-    }
+public record BehandlingUuidDto(@NotNull @Valid UUID behandlingUuid) {
 
     public BehandlingUuidDto(String behandlingUuid) {
         this(UUID.fromString(behandlingUuid));
