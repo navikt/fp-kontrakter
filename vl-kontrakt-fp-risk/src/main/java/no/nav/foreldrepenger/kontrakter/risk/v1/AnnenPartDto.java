304e7102088b60436e2d3d646dac506c69d9620d
Petter Ekern <43809623+pekern@users.noreply.github.com>
2022-02-15 13:50:37 +0100

    FIX: Legger på validering på utenlands fnr. Selv om dette er veldig fritt er det greit å ha en viss kontroll (#332)

--- vl-kontrakt-fp-risk/src/main/java/no/nav/foreldrepenger/kontrakter/risk/v1/AnnenPartDto.java
+++ vl-kontrakt-fp-risk/src/main/java/no/nav/foreldrepenger/kontrakter/risk/v1/AnnenPartDto.java
@@ -3,10 +3,11 @@ package no.nav.foreldrepenger.kontrakter.risk.v1;
 import java.util.Optional;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 
 import no.nav.foreldrepenger.kontrakter.risk.kodeverk.AktørId;
 
-public record AnnenPartDto(@Valid AktørId annenpartAktørId, String utlandskFnr) {
+public record AnnenPartDto(@Valid AktørId annenpartAktørId, @Pattern(regexp = "[a-zA-ZæøåÆØÅ0-9]{0,200}") String utlandskFnr) {
 
     @Deprecated // Bruk annenpartAktørId()
     public Optional<AktørId> getAnnenPartAktørId() {
