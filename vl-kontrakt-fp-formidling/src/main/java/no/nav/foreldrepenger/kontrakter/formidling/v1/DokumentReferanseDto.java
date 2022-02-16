79b2385446259bfc4bef1855daa97735ab50af4e
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2022-02-16 13:45:24 +0100

    TFP-4885 tilpasse rest-kvittering til single journalpost (#331)

--- vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v1/DokumentReferanseDto.java
+++
@@ -1,9 +0,0 @@
-package no.nav.foreldrepenger.kontrakter.formidling.v1;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
-public record DokumentReferanseDto(@NotNull String linkTekst,
-                                   @NotNull @Pattern(regexp = "^[\\p{L}\\p{N}_\\.\\-]+$") String journalpostId,
-                                   @NotNull String dokumentId) {
-}
