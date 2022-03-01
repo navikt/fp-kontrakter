8a72da5dc11ecffc01cdd5cd7fc71614ab1c2def
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-03-01 10:37:13 +0100

    Forenkle kontrakter rundt formidling

--- vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/kodeverk/EnumDokumentMalRestriksjon.java
+++
@@ -1,8 +0,0 @@
-package no.nav.foreldrepenger.kontrakter.formidling.kodeverk;
-
-public enum EnumDokumentMalRestriksjon {
-    INGEN,
-    REVURDERING,
-    ÅPEN_BEHANDLING,
-    ÅPEN_BEHANDLING_IKKE_SENDT
-}
