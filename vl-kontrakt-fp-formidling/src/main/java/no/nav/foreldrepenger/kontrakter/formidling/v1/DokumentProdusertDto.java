af5e0a29118b4a617eb4aef0674326d4a636956a
Michal J. Sladek <mrsladek@users.noreply.github.com>
2022-03-05 22:43:23 +0100

    Sanerer historikk aktør som brukes ikke i formidling. (#341)

--- vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v1/DokumentProdusertDto.java
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v1/DokumentProdusertDto.java
@@ -3,15 +3,11 @@ package no.nav.foreldrepenger.kontrakter.formidling.v1;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
-import no.nav.foreldrepenger.kontrakter.formidling.kodeverk.HistorikkAktør;
-
 public record DokumentProdusertDto(@NotNull UUID behandlingUuid,
                                    @NotNull UUID dokumentbestillingUuid,
-                                   @Valid HistorikkAktør historikkAktør,
                                    @NotNull @Pattern(regexp = "[A-Z]{6}") String dokumentMal,
                                    @NotNull @Pattern(regexp = "^[\\p{L}\\p{N}_\\.\\-]+$") String journalpostId,
                                    @NotNull String dokumentId,
