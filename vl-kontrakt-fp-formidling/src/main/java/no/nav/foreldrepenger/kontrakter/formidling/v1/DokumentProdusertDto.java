9ad45b2f2eaace5c50662774d000f7b82f35557f
Michal J. Sladek <michal.sladek@nav.no>
2022-03-07 09:35:33 +0100

    Renser DokumentProdusertDto

--- vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v1/DokumentProdusertDto.java
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v1/DokumentProdusertDto.java
@@ -1,6 +1,5 @@
 package no.nav.foreldrepenger.kontrakter.formidling.v1;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 import javax.validation.constraints.NotNull;
@@ -10,8 +9,7 @@ public record DokumentProdusertDto(@NotNull UUID behandlingUuid,
                                    @NotNull UUID dokumentbestillingUuid,
                                    @NotNull @Pattern(regexp = "[A-Z]{6}") String dokumentMal,
                                    @NotNull @Pattern(regexp = "^[\\p{L}\\p{N}_\\.\\-]+$") String journalpostId,
-                                   @NotNull String dokumentId,
-                                   @NotNull LocalDateTime opprettetTidspunkt) {
+                                   @NotNull String dokumentId) {
 
 
 
