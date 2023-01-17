89a32392804a37cacc4f01024c161a6f37aff98b
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2023-01-17 11:29:55 +0100

    TFP-5333: AnnullerKravGrunnlagDto trenger bare sende over vedtaksId for kravgrunnlaget (#410)

--- vl-kontrakt-fp-ws-proxy/src/main/java/no/nav/foreldrepenger/kontrakter/fpwsproxy/tilbakekreving/kravgrunnlag/request/AnnullerKravGrunnlagDto.java
+++ vl-kontrakt-fp-ws-proxy/src/main/java/no/nav/foreldrepenger/kontrakter/fpwsproxy/tilbakekreving/kravgrunnlag/request/AnnullerKravGrunnlagDto.java
@@ -4,9 +4,6 @@ import java.math.BigInteger;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
-public record AnnullerKravGrunnlagDto(@NotNull @Pattern(regexp = "^[A-Z0-9]{1}$") String kodeAksjon,
-                                      @NotNull @Digits(integer = 14, fraction = 0) BigInteger vedtakId,
-                                      @NotNull @Pattern(regexp = "^[a-zA-ZæøåÆØÅ0-9]{7}$") String saksbehId) {
+public record AnnullerKravGrunnlagDto(@NotNull @Digits(integer = 14, fraction = 0) BigInteger vedtakId) {
 }
