0502d40776d78ff769e664cb9a0f487f749942ad
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2023-01-19 16:06:40 +0100

    TFP-5333: Rydder opp i inputvalidering av tilbakekrevingsVedtakDto (#415)
    * TFP-5333: Rydder opp i inputvalidering av tilbakekrevingsVedtakDto
    
    * Fjerner ubrukt import

--- vl-kontrakt-fp-ws-proxy/src/main/java/no/nav/foreldrepenger/kontrakter/fpwsproxy/tilbakekreving/iverksett/TilbakekrevingsperiodeDTO.java
+++ vl-kontrakt-fp-ws-proxy/src/main/java/no/nav/foreldrepenger/kontrakter/fpwsproxy/tilbakekreving/iverksett/TilbakekrevingsperiodeDTO.java
@@ -6,23 +6,20 @@ import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import no.nav.foreldrepenger.kontrakter.fpwsproxy.tilbakekreving.kravgrunnlag.respons.Periode;
 
 public record TilbakekrevingsperiodeDTO(@Valid @NotNull Periode periode,
-                                        @Pattern(regexp = "^[\\p{L}\\p{Blank}.'\\-\\–0-9/\\p{Space}%§\\!?@_()+:;,=\"&\\p{Sc}]*$") String renterBeregnes,
                                         @Digits(integer = 8, fraction = 2) BigDecimal belopRenter,
                                         @Valid @NotNull @Size List<@Valid @NotNull TilbakekrevingsbelopDTO> tilbakekrevingsbelop) {
 
     public TilbakekrevingsperiodeDTO(Builder builder) {
-        this(builder.periode, builder.renterBeregnes, builder.belopRenter, builder.tilbakekrevingsbelop);
+        this(builder.periode, builder.belopRenter, builder.tilbakekrevingsbelop);
     }
 
     public static class Builder {
         private Periode periode;
-        private String renterBeregnes;
         private BigDecimal belopRenter;
         private List<TilbakekrevingsbelopDTO> tilbakekrevingsbelop;
 
@@ -31,11 +28,6 @@ public record TilbakekrevingsperiodeDTO(@Valid @NotNull Periode periode,
 
         public Builder periode(Periode periode) {
             this.periode = periode;
-            return this;
-        }
-
-        public Builder renterBeregnes(String renterBeregnes) {
-            this.renterBeregnes = renterBeregnes;
             return this;
         }
 
