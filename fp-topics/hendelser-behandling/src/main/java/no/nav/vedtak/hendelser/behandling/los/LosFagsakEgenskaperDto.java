261ce16d8336b8d16b90207b72b065240a33d917
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-03-09 09:54:28 +0100

    Trenger SED-flagg i fagsakegenskaper

--- fp-topics/hendelser-behandling/src/main/java/no/nav/vedtak/hendelser/behandling/los/LosFagsakEgenskaperDto.java
+++ fp-topics/hendelser-behandling/src/main/java/no/nav/vedtak/hendelser/behandling/los/LosFagsakEgenskaperDto.java
@@ -1,6 +1,7 @@
 package no.nav.vedtak.hendelser.behandling.los;
 
-public record LosFagsakEgenskaperDto(UtlandMarkering utlandMarkering) {
+public record LosFagsakEgenskaperDto(Boolean skalInnhenteSED,
+                                     UtlandMarkering utlandMarkering) {
 
     public enum UtlandMarkering {
         NASJONAL, EØS_BOSATT_NORGE, BOSATT_UTLAND
