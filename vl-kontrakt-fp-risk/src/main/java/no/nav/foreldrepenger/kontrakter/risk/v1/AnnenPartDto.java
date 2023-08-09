c5efbfcf6b6cf8da10fff2d3ae697c89c5f05d56
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-08-09 13:17:20 +0200

    Tillate unicode punctuation i utlandsk fnr

--- vl-kontrakt-fp-risk/src/main/java/no/nav/foreldrepenger/kontrakter/risk/v1/AnnenPartDto.java
+++ vl-kontrakt-fp-risk/src/main/java/no/nav/foreldrepenger/kontrakter/risk/v1/AnnenPartDto.java
@@ -7,15 +7,6 @@ import javax.validation.constraints.Pattern;
 
 import no.nav.foreldrepenger.kontrakter.risk.kodeverk.AktørId;
 
-public record AnnenPartDto(@Valid AktørId annenpartAktørId, @Pattern(regexp = "^[\\p{Graph}\\p{Space}\\p{Sc}\\p{L}\\p{M}\\p{N}]+$") String utlandskFnr) {
-
-    @Deprecated // Bruk annenpartAktørId()
-    public Optional<AktørId> getAnnenPartAktørId() {
-        return Optional.ofNullable(annenpartAktørId);
-    }
-    @Deprecated // Bruk utlandskFnr()
-    public Optional<String> getUtlandskFnr() {
-        return Optional.ofNullable(utlandskFnr);
-    }
+public record AnnenPartDto(@Valid AktørId annenpartAktørId, @Pattern(regexp = "^[\\p{Graph}\\p{Space}\\p{Sc}\\p{L}\\p{M}\\p{N}\\p{P}]+$") String utlandskFnr) {
 
 }
