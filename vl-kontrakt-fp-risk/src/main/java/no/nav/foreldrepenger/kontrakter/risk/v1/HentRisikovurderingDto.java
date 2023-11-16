6931b638e8358b95ae6b64a97dd7c5a1c2de7a15
Petter Ekern <petter.ekern@nav.no>
2023-11-16 08:56:22 +0100

    TFP-5630: Utvider fprisk kontrakt med mulighet for saksnummer

--- vl-kontrakt-fp-risk/src/main/java/no/nav/foreldrepenger/kontrakter/risk/v1/HentRisikovurderingDto.java
+++ vl-kontrakt-fp-risk/src/main/java/no/nav/foreldrepenger/kontrakter/risk/v1/HentRisikovurderingDto.java
@@ -4,5 +4,5 @@ import java.util.UUID;
 
 import jakarta.validation.constraints.NotNull;
 
-public record HentRisikovurderingDto(@NotNull UUID konsumentId) {
-}
+// TODO Sett @NotNull på saksnummer når fpsak og fprisk tar i bruk ny kontrakt
+public record HentRisikovurderingDto(@NotNull UUID konsumentId, String saksnummer) {}
--- vl-kontrakt-fp-risk/src/main/java/no/nav/foreldrepenger/kontrakter/risk/v1/RisikovurderingRequestDto.java
+++ vl-kontrakt-fp-risk/src/main/java/no/nav/foreldrepenger/kontrakter/risk/v1/RisikovurderingRequestDto.java
@@ -10,11 +10,13 @@ import jakarta.validation.constraints.NotNull;
 import no.nav.foreldrepenger.kontrakter.risk.kodeverk.AktørId;
 import no.nav.foreldrepenger.kontrakter.risk.kodeverk.YtelseType;
 
+// TODO Sett @NotNull på saksnummer når fpsak og fprisk tar i bruk ny kontrakt
 public record RisikovurderingRequestDto(@NotNull @Valid AktørId søkerAktørId,
                                         @NotNull LocalDate skjæringstidspunkt,
                                         @NotNull LocalDate opplysningsperiodeFom,
                                         LocalDate opplysningsperiodeTom,
                                         @NotNull UUID konsumentId,
+                                        String saksnummer,
                                         @NotNull YtelseType ytelseType,
                                         @Valid AnnenPartDto annenPart) {
 
