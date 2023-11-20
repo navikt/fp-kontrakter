9d4bd2ebca62664c837a2f1c2cdaf523a3bda42f
Michal J. Sladek <mrsladek@users.noreply.github.com>
2023-11-20 13:44:18 +0100

    TFP-5422: Opprett sak v2 fra fordel (#492)

---
+++ vl-kontrakt-fordel/src/main/java/no/nav/foreldrepenger/kontrakter/fordel/OpprettSakV2Dto.java
@@ -0,0 +1,10 @@
+package no.nav.foreldrepenger.kontrakter.fordel;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+
+public record OpprettSakV2Dto(@Digits(integer = 18, fraction = 0) String journalpostId,
+                              @NotNull @Valid YtelseTypeDto ytelseType,
+                              @NotNull @Digits(integer = 19, fraction = 0) String aktørId) {
+}
