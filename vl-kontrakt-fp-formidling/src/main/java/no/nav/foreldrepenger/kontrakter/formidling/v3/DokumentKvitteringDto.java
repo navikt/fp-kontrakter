6999c285333e1d2b69b2131f2c4e7b272ed11f93
Michal J. Sladek <mrsladek@users.noreply.github.com>
2024-01-30 08:28:33 +0100

    TFP-5668: Forenkler kvittering kontrakten. (#513)

--- vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v3/DokumentKvitteringDto.java
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v3/DokumentKvitteringDto.java
@@ -13,13 +13,11 @@ import no.nav.foreldrepenger.kontrakter.formidling.kodeverk.DokumentMal;
  * Brukes til å kvittere tilbake til bestilleren at brevet ble produsert, journalført og distribuert.
  * @param behandlingUuid - referanse til behandling det ble bestillt brev for.
  * @param dokumentbestillingUuid - en unik bestillings id som ble brukt ved bestilling.
- * @param dokumentMal - dokument mal som ble brukt i bestillingen.
  * @param journalpostId - journalpost id hvor brevet er arkivert.
  * @param dokumentId - dokument id som identifiserer dokumentet i journalposten.
  */
 public record DokumentKvitteringDto(@NotNull @Valid UUID behandlingUuid,
                                     @NotNull @Valid UUID dokumentbestillingUuid,
-                                    @NotNull @Valid DokumentMal dokumentMal,
                                     @NotNull @Pattern(regexp = "^[\\p{L}\\p{N}_.\\-]+$") String journalpostId,
                                     @NotNull @Digits(integer = 12, fraction = 0) String dokumentId) {
 
