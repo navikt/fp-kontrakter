983816dd342a33f31f2cd6451412d824331cd53a
Petter Ekern <43809623+pekern@users.noreply.github.com>
2024-03-05 14:34:21 +0100

    TFP-5719: Kontrakt med status på manglende / mottatte inntektsmelding… (#525)
    TFP-5719: Kontrakt med status på manglende / mottatte inntektsmeldinger for formidling til å bruke i brev

---
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/fpsak/inntektsmeldinger/ArbeidsforholdInntektsmeldinger.java
@@ -0,0 +1,18 @@
+package no.nav.foreldrepenger.kontrakter.fpsak.inntektsmeldinger;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record ArbeidsforholdInntektsmeldinger(@NotNull List<@Valid @NotNull ArbeidsforholdInntektsmelding> arbeidsforholdInntektsmelding) {
+
+    public record ArbeidsforholdInntektsmelding(@NotNull @Pattern(regexp = "\\d{9}|\\d{13}") String arbeidsgiverIdent,
+                                                @NotNull @Digits(integer = 5, fraction = 2) @DecimalMin("0.00") @DecimalMax("100.00") BigDecimal stillingsprosent,
+                                                @NotNull Boolean erInntektsmeldingMottatt) {}
+}
