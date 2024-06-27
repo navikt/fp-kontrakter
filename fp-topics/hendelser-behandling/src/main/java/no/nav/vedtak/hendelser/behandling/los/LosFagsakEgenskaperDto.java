62c89bc72145a18c8301d1af2dbc8d707f21b387
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-06-27 19:45:49 +0200

    Trigge release nok en gang

--- fp-topics/hendelser-behandling/src/main/java/no/nav/vedtak/hendelser/behandling/los/LosFagsakEgenskaperDto.java
+++ fp-topics/hendelser-behandling/src/main/java/no/nav/vedtak/hendelser/behandling/los/LosFagsakEgenskaperDto.java
@@ -2,4 +2,7 @@ package no.nav.vedtak.hendelser.behandling.los;
 
 import java.util.List;
 
-public record LosFagsakEgenskaperDto(List<String> saksegenskaper, @Deprecated String fagsakMarkering) { }
+public record LosFagsakEgenskaperDto(List<String> saksegenskaper,
+                                     @Deprecated String fagsakMarkering) {
+    
+}
