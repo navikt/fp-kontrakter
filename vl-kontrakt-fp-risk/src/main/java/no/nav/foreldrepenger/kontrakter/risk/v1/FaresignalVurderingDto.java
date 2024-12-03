571b082553360cb841f736d93321cf607c20f79d
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2024-12-03 20:26:31 +0100

    Et par ekstra filer

--- vl-kontrakt-fp-risk/src/main/java/no/nav/foreldrepenger/kontrakter/risk/v1/FaresignalVurderingDto.java
+++
@@ -1,7 +0,0 @@
-package no.nav.foreldrepenger.kontrakter.risk.v1;
-
-import no.nav.foreldrepenger.kontrakter.risk.kodeverk.FaresignalVurdering;
-
-import jakarta.validation.constraints.NotNull;
-
-public record FaresignalVurderingDto(@NotNull FaresignalVurdering faresignalVurdering) {}
