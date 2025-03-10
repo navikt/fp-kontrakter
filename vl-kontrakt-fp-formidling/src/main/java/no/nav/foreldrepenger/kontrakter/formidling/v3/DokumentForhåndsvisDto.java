2c92627cae1bd40770374e23e8316db731934895
espenwaaga <espen.waaga@nav.no>
2025-03-10 12:16:01 +0100

    Oppdatere kontrakt ved å gjenbruke fritekst

--- vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v3/DokumentForhåndsvisDto.java
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v3/DokumentForhåndsvisDto.java
@@ -15,8 +15,7 @@ import java.util.UUID;
  * @param tittel Tittel/overskrift i brevet, gjelder fritekst
  * @param fritekst Fritekst innhold i brevet, gjelder fritekst
  * @param revurderingÅrsak brukes kun om VARSEL_OM_REVURDERING er valgt.
- * @param fritekst kun brev som har eller er fritekst, dvs INNHENTE_OPPLYSNINGER og VARSEL_OM_REVURDERING om årsak ANNET er valgt, brødtekst i FRITEKSTBREV.
- * @param fritekstHtml kun brev som har eller er fritekst, dvs INNHENTE_OPPLYSNINGER og VARSEL_OM_REVURDERING om årsak ANNET er valgt, brødtekst i FRITEKSTBREV.
+ * @param fritekst kun brev som har eller er fritekst, dvs INNHENTE_OPPLYSNINGER og VARSEL_OM_REVURDERING om årsak ANNET er valgt, brødtekst i FRITEKSTBREV eller html i FRITEKSTBREV_HTML
  * @param tittel tittel i FRITEKSTBREV eller FRITEKSTBREV_HTML dokumentet. Kun om FRITEKSTBREV eller FRITEKSTBREV_HTML mal brukes.
  */
 public record DokumentForhåndsvisDto(@NotNull @Valid UUID behandlingUuid,
@@ -24,7 +23,6 @@ public record DokumentForhåndsvisDto(@NotNull @Valid UUID behandlingUuid,
                                      @NotNull @Valid DokumentMal dokumentMal,
                                      @Valid RevurderingÅrsak revurderingÅrsak,
                                      String tittel,
-                                     String fritekst,
-                                     String fritekstHtml) {
+                                     String fritekst) {
 
 }
