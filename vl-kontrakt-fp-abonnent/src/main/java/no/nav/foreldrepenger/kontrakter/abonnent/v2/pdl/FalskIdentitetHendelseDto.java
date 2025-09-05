83f064a3fb78ae1536ed9725b5e27bef653d3b08
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-09-05 23:24:46 +0200

    TFP-6380 Leesah Falsk Identitet

---
+++ vl-kontrakt-fp-abonnent/src/main/java/no/nav/foreldrepenger/kontrakter/abonnent/v2/pdl/FalskIdentitetHendelseDto.java
@@ -0,0 +1,52 @@
+package no.nav.foreldrepenger.kontrakter.abonnent.v2.pdl;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import no.nav.foreldrepenger.kontrakter.abonnent.v2.AktørIdDto;
+import no.nav.foreldrepenger.kontrakter.abonnent.v2.HendelseDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FalskIdentitetHendelseDto extends HendelseDto {
+
+    public static final String HENDELSE_TYPE = "FALSKID";
+    public static final String AVSENDER = "PDL";
+
+    @NotNull
+    @Size(min = 1)
+    private List<AktørIdDto> aktørId;
+
+    private boolean erFalsk;
+
+    public void setAktørId(List<AktørIdDto> aktørId) {
+        this.aktørId = aktørId;
+    }
+
+    public List<AktørIdDto> getAktørId() {
+        return this.aktørId;
+    }
+
+    public boolean isErFalsk() {
+        return erFalsk;
+    }
+
+    public void setErFalsk(boolean erFalsk) {
+        this.erFalsk = erFalsk;
+    }
+
+    @Override
+    public String getHendelsetype() {
+        return HENDELSE_TYPE;
+    }
+
+    @Override
+    public String getAvsenderSystem() {
+        return AVSENDER;
+    }
+
+    @Override
+    public List<String> getAlleAktørId() {
+        return aktørId.stream().map(AktørIdDto::getAktørId).collect(Collectors.toList());
+    }
+}
