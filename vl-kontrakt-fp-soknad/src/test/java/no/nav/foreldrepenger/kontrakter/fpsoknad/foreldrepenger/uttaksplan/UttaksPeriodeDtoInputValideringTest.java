a3f675bd11a1ae2c3b2c341375e743a46249ce00
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2025-11-05 08:26:02 +0100

    Chore: Legger til strengere validering av uttaksperiode vedrørende sa… (#667)
    * Chore: Legger til strengere validering av uttaksperiode vedrørende samtidig uttak
    
    * Legge rogså til happy case test for validering

---
+++ vl-kontrakt-fp-soknad/src/test/java/no/nav/foreldrepenger/kontrakter/fpsoknad/foreldrepenger/uttaksplan/UttaksPeriodeDtoInputValideringTest.java
@@ -0,0 +1,61 @@
+package no.nav.foreldrepenger.kontrakter.fpsoknad.foreldrepenger.uttaksplan;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import no.nav.foreldrepenger.kontrakter.fpsoknad.builder.UttakplanPeriodeBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UttaksPeriodeDtoInputValideringTest {
+
+    @Test
+    void uttaksperiode_ønsker_samtidig_uttak_uten_prosent_skal_føre_til_valideringsfeil() {
+        var uttaksperiode = UttakplanPeriodeBuilder
+                .uttak(KontoType.FEDREKVOTE, LocalDate.now().minusWeeks(4), LocalDate.now())
+                .medØnskerSamtidigUttak(true)
+                .medSamtidigUttakProsent(null)
+                .build();
+
+        var validator = hentValidator();
+        var resultat = validator.validate(uttaksperiode);
+        assertThat(resultat).hasSize(1)
+                .extracting(ConstraintViolation::getMessage)
+                .contains("ønskerSamtidigUttak er satt, men ikke prosent, eller så er prosent satt og ikke ønskerSamtidigUttak");
+    }
+
+
+    @Test
+    void uttaksperiode_ønsker_samtidig_uttak_med_prosent_skal_ikke_føre_til_valideringsfeil() {
+        var uttaksperiode = UttakplanPeriodeBuilder
+                .uttak(KontoType.FEDREKVOTE, LocalDate.now().minusWeeks(4), LocalDate.now())
+                .medØnskerSamtidigUttak(true)
+                .medSamtidigUttakProsent(20.2)
+                .build();
+
+        var validator = hentValidator();
+        var resultat = validator.validate(uttaksperiode);
+        assertThat(resultat).isEmpty();
+    }
+
+    @Test
+    void uttaksperiode_ikke_samtidig_uttak_skal_ikke_føre_til_valideringsfeil() {
+        var uttaksperiode = UttakplanPeriodeBuilder
+                .uttak(KontoType.FEDREKVOTE, LocalDate.now().minusWeeks(4), LocalDate.now())
+                .build();
+
+        var validator = hentValidator();
+        var resultat = validator.validate(uttaksperiode);
+        assertThat(resultat).isEmpty();
+
+    }
+
+    private static Validator hentValidator() {
+        try (var factory = Validation.buildDefaultValidatorFactory()) {
+            return factory.getValidator();
+        }
+    }
+}
