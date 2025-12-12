a094c8aacb4a5aed587eca3e21c09bb60d697cc1
Petter Ekern <43809623+pekern@users.noreply.github.com>
2025-12-12 14:40:40 +0100

    TFP-6518: Kontrakt for å sende beregningsgrunnlag fra fpoversikt til … (#683)
    TFP-6518: Kontrakt for å sende beregningsgrunnlag fra fpoversikt til frontend

---
+++ vl-kontrakt-felles/src/main/java/no/nav/foreldrepenger/kontrakter/felles/kodeverk/AktivitetStatus.java
@@ -0,0 +1,19 @@
+package no.nav.foreldrepenger.kontrakter.felles.kodeverk;
+
+public enum AktivitetStatus {
+    ARBEIDSAVKLARINGSPENGER,
+    ARBEIDSTAKER,
+    DAGPENGER,
+    FRILANSER,
+    MILITÆR_ELLER_SIVIL,
+    SELVSTENDIG_NÆRINGSDRIVENDE,
+    KOMBINERT_AT_FL,
+    KOMBINERT_AT_SN,
+    KOMBINERT_FL_SN,
+    KOMBINERT_AT_FL_SN,
+    BRUKERS_ANDEL,
+    KUN_YTELSE,
+    TTLSTØTENDE_YTELSE,
+    VENTELØNN_VARTPENGER
+
+}
