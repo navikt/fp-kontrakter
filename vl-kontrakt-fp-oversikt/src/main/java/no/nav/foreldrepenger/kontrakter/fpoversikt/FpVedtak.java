2b842d63a2a8857470eb531ac06383c5f6038ba3
johannbm <johannes.moskvil@gmail.com>
2026-02-09 16:58:59 +0100

    Oppdater annotasjoner

--- vl-kontrakt-fp-oversikt/src/main/java/no/nav/foreldrepenger/kontrakter/fpoversikt/FpVedtak.java
+++ vl-kontrakt-fp-oversikt/src/main/java/no/nav/foreldrepenger/kontrakter/fpoversikt/FpVedtak.java
@@ -8,5 +8,5 @@ import java.util.List;
 public record FpVedtak(@NotNull List<@NotNull @Valid UttakPeriode> perioder,
                        List<@NotNull @Valid UttakPeriodeAnnenpartEøs> perioderAnnenpartEøs,
                        @Valid Beregningsgrunnlag beregningsgrunnlag,
-                       @NotNull TilkjentYtelse tilkjentYtelse) {
+                       @NotNull @Valid TilkjentYtelse tilkjentYtelse) {
 }
