77c3fef53a88aa6ec6aa1f6db468541a5b1b1695
Anders Palfi <anders.palfi@nav.no>
2026-03-12 11:07:10 +0100

    Chore: aktivitet gradering kan være null for annen part

--- vl-kontrakt-fp-oversikt/src/main/java/no/nav/foreldrepenger/kontrakter/fpoversikt/Gradering.java
+++ vl-kontrakt-fp-oversikt/src/main/java/no/nav/foreldrepenger/kontrakter/fpoversikt/Gradering.java
@@ -2,5 +2,5 @@ package no.nav.foreldrepenger.kontrakter.fpoversikt;
 
 import jakarta.validation.constraints.NotNull;
 
-public record Gradering(@NotNull Arbeidstidprosent arbeidstidprosent, @NotNull Aktivitet aktivitet) {
+public record Gradering(@NotNull Arbeidstidprosent arbeidstidprosent, Aktivitet aktivitet) {
 }
