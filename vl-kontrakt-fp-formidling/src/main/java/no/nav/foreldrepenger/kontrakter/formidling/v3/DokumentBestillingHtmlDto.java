8fc7de97a68c5c012e048044ff9545661f7a9696
Anders Palfi <anders.palfi@nav.no>
2026-04-09 11:22:06 +0200

    temp

--- vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v3/DokumentBestillingHtmlDto.java
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v3/DokumentBestillingHtmlDto.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.kontrakter.formidling.v3;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import no.nav.foreldrepenger.kontrakter.formidling.kodeverk.DokumentMal;
+import no.nav.foreldrepenger.kontrakter.formidling.kodeverk.RevurderingÅrsak;
 import no.nav.foreldrepenger.kontrakter.formidling.kodeverk.Saksnummer;
 
 import java.util.UUID;
@@ -10,9 +11,14 @@ import java.util.UUID;
 /**
  * Brukes for generering av html fra automatisk brev.
  * @param behandlingUuid referanse til behandling det skal sendes brev for.
- * @param dokumentMal dokument mal som skal brukes til å forhåndsvise brevet.
+ * @param saksnummer saksnummeret til fagsaken.
+ * @param dokumentMal dokument mal som skal brukes til å generere brevet.
+ * @param revurderingÅrsak valgfri årsak til revurdering, brukes for brevmaler som krever det (f.eks. VARSEL_OM_REVURDERING).
+ * @param fritekst (VARSEL_OM_REVURDERING).
  */
 public record DokumentBestillingHtmlDto(@NotNull @Valid UUID behandlingUuid,
                                         @NotNull @Valid Saksnummer saksnummer,
-                                        @NotNull @Valid DokumentMal dokumentMal) {
+                                        @NotNull @Valid DokumentMal dokumentMal,
+                                        @Valid RevurderingÅrsak revurderingÅrsak,
+                                        String fritekst) {
 }
