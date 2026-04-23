3cce27e043afa3d5946736775c1ce270d022ee26
Anders Palfi <anders.palfi@nav.no>
2026-04-23 11:20:56 +0200

    TFP-6877 spesialfelt for støtte for html generering for varsel om revurdering

--- vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v3/DokumentBestillingHtmlDto.java
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v3/DokumentBestillingHtmlDto.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.kontrakter.formidling.v3;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import no.nav.foreldrepenger.kontrakter.formidling.kodeverk.DokumentMal;
+import no.nav.foreldrepenger.kontrakter.formidling.kodeverk.RevurderingÅrsak;
 import no.nav.foreldrepenger.kontrakter.formidling.kodeverk.Saksnummer;
 
 import java.util.UUID;
@@ -11,8 +12,10 @@ import java.util.UUID;
  * Brukes for generering av html fra automatisk brev.
  * @param behandlingUuid referanse til behandling det skal sendes brev for.
  * @param dokumentMal dokument mal som skal brukes til å forhåndsvise brevet.
+ * @param revurderingÅrsak brukes kun om VARSEL_OM_REVURDERING er valgt.
  */
 public record DokumentBestillingHtmlDto(@NotNull @Valid UUID behandlingUuid,
                                         @NotNull @Valid Saksnummer saksnummer,
-                                        @NotNull @Valid DokumentMal dokumentMal) {
+                                        @NotNull @Valid DokumentMal dokumentMal,
+                                        @Valid RevurderingÅrsak revurderingÅrsak) {
 }
