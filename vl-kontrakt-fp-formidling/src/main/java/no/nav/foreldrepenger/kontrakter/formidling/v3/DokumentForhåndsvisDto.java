c920be51677ebd6401d314e5e7f74e0619ae433d
Anders Palfi <anders.palfi@nav.no>
2026-06-04 08:23:31 +0200

    Chore: fjerner ubrukt tittel felt i forhåndsvisning dto for brev

--- vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v3/DokumentForhåndsvisDto.java
+++ vl-kontrakt-fp-formidling/src/main/java/no/nav/foreldrepenger/kontrakter/formidling/v3/DokumentForhåndsvisDto.java
@@ -12,17 +12,13 @@ import java.util.UUID;
  * Brukes for forhåndsvisning av brev.
  * @param behandlingUuid referanse til behandling det skal sendes brev for.
  * @param dokumentMal dokument mal som skal brukes til å forhåndsvise brevet.
- * @param tittel Tittel/overskrift i brevet, gjelder fritekst
- * @param fritekst Fritekst innhold i brevet, gjelder fritekst
  * @param revurderingÅrsak brukes kun om VARSEL_OM_REVURDERING er valgt.
- * @param fritekst kun brev som har eller er fritekst, dvs INNHENTE_OPPLYSNINGER og VARSEL_OM_REVURDERING om årsak ANNET er valgt, brødtekst i FRITEKSTBREV eller html i FRITEKSTBREV_HTML
- * @param tittel tittel i FRITEKSTBREV eller FRITEKSTBREV_HTML dokumentet. Kun om FRITEKSTBREV eller FRITEKSTBREV_HTML mal brukes.
+ * @param fritekst kun brev som har eller er fritekst, dvs INNHENTE_OPPLYSNINGER og VARSEL_OM_REVURDERING om årsak ANNET er valgt, brødtekst i FRITEKSTBREV_HTML
  */
 public record DokumentForhåndsvisDto(@NotNull @Valid UUID behandlingUuid,
                                      @NotNull @Valid Saksnummer saksnummer,
                                      @NotNull @Valid DokumentMal dokumentMal,
                                      @Valid RevurderingÅrsak revurderingÅrsak,
-                                     String tittel,
                                      String fritekst) {
 
 }
