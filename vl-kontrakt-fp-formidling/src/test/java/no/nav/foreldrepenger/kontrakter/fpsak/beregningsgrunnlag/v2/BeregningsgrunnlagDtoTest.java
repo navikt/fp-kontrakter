0346cc35fe328f2fd1799b521c53f74a48f354a8
anjaaalerud <anja.gotesen.aalerud@nav.no>
2026-09-04 15:26:55 +0200

    TFP-5197 fikser test

--- vl-kontrakt-fp-formidling/src/test/java/no/nav/foreldrepenger/kontrakter/fpsak/beregningsgrunnlag/v2/BeregningsgrunnlagDtoTest.java
+++ vl-kontrakt-fp-formidling/src/test/java/no/nav/foreldrepenger/kontrakter/fpsak/beregningsgrunnlag/v2/BeregningsgrunnlagDtoTest.java
@@ -45,7 +45,7 @@ class BeregningsgrunnlagDtoTest {
                                 LocalDate.now(),
                                 new BgAndelArbeidsforholdDto(testIdent, testRef, BigDecimal.ZERO, BigDecimal.ZERO),
                                 false,
-                                perÅr)))
+                                perÅr))
                         )),
                 false,
                 false); //denne vil alltid være false så lenge erBesteberegnet er false
@@ -69,6 +69,7 @@ class BeregningsgrunnlagDtoTest {
         assertThat(roundTripped.beregningsgrunnlagperioder().getFirst().beregningsgrunnlagandeler()).hasSize(1);
         assertThat(roundTripped.beregningsgrunnlagperioder().getFirst().beregningsgrunnlagandeler().getFirst().avkortetPrÅr()).isEqualTo(perÅr);
         assertThat(roundTripped.beregningsgrunnlagperioder().getFirst().beregningsgrunnlagandeler().getFirst().bruttoPrÅr()).isEqualTo(perÅr);
+        assertThat(roundTripped.beregningsgrunnlagperioder().getFirst().beregningsgrunnlagandeler().getFirst().beregnetPrÅr()).isEqualTo(perÅr);
         assertThat(roundTripped.beregningsgrunnlagperioder().getFirst().beregningsgrunnlagandeler().getFirst().aktivitetStatus()).isEqualTo(AktivitetStatusDto.ARBEIDSTAKER);
         assertThat(roundTripped.beregningsgrunnlagperioder().getFirst().beregningsgrunnlagandeler().getFirst().arbeidsforholdType()).isEqualTo(OpptjeningAktivitetDto.ARBEID);
         assertThat(roundTripped.beregningsgrunnlagperioder().getFirst().beregningsgrunnlagandeler().getFirst().arbeidsforhold().arbeidsgiverIdent()).isEqualTo(testIdent);
